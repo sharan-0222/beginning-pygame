7388f03c386aea420793d73de8dcb2e456981ae7
sharan-0222 <sharanmenonc@gmail.com>
2025-05-15 19:26:52 +0530

    Add files via upload

---
+++ act1.py
@@ -0,0 +1,12 @@
+import pygame 
+
+pygame.init()
+done=False
+pygame.display.set_mode((400,500))
+
+while True:
+    for event in pygame.event.get():
+        if event.type==pygame.QUIT:
+            pygame.quit()
+
+    pygame.display.flip()
